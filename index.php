3c8282ad6e02d302c6701c062ada61dc50993061
raggabatz <erik6594@gmail.com>
2020-09-23 05:15:02 -0600

    Index para visualización de productos para compra

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include ("header.php")?>
+<article id="mainArticle">
+    <div id="index">
+        <div class="items-header">
+            <p>Search Nav</p>
+        </div>
+        <div class="items">
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+            <?php include ("shop-item.php")?>
+        </div>
+    </div>
+</article>
+<?php include ("menu.php")?>
+<?php include ("footer.php")?>
