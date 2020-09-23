63bcfe0c0fbde5f712cf0e4d6efcccd0be217bcf
raggabatz <erik6594@gmail.com>
2020-09-22 21:06:25 -0600

    Reorganizacion de codigo de encabezado, menu y pie de pagina

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+    <script src="https://kit.fontawesome.com/7bf3b56285.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parcial 2</title>
+    <link rel="stylesheet" href="css/main.css">
+    <script type="text/javascript" src="js/script.js"></script>
+</head>
+<body>
+    <header id="pageHeader">
+        <img src="res/umg.png" alt="" class="logo">
+    </header>
--- menu.php
+++ menu.php
@@ -1,27 +1,9 @@
-<?php
-function menu(){
-    $html = "";
-    $html .= "<nav id=\"mainNav\">\n";
-    $html .= "<a href=\"home.php\">Inicio</a>\n";
-    $html .= "<a href=\"listadoProductos.php\">Productos</a>\n";
-    $html .= "<a href=\"listadoProvedores.php\">Proveedores</a>\n";
-    $html .= "<a href=\"listadoSucursales.php\">Sucursales</a>\n";
-    $html .= "<a href=\"listadoUsuarios.php\">Usuarios</a>\n";
-    $html .= "<a href=\"listadoOrdenes.php\">Ordenes</a>\n";
-    $html .= "<a href=\"listadoInventario.php\">Inventario</a>\n";
-    $html .= "</nav>\n";
-    $html .= "<footer id=\"pageFooter\">\n";
-    $html .= "<i class=\"fas fa-user-astronaut\"></i>\n";
-    $html .= "<label>Erick Ricardo Batz Cuscul 090-14-4920</label>\n";
-    $html .= "<i class=\"fas fa-user-nurse\"></i>\n";
-    $html .= "<label>Sebastian Lorenzo Lopez</label>\n";
-    $html .= "<i class=\"fas fa-user-secret\"></i>\n";
-    $html .= "<label>Luis Andre Morales Acuña</label>\n";
-    $html .= "<i class=\"fas fa-user-graduate\"></i>\n";
-    $html .= "<label>Joseline Maria Morales Samayoa</label>\n";
-    $html .= "</footer>\n";
-
-    echo $html;
-}
-
-?>
+<nav id="mainNav"> 
+    <a href="index.php"><img src="res/home.png" title="Inicio"></a>
+    <a href="listadoProductos.php"><img src="res/product.png" title="Producto"></a>
+    <a href="listadoProvedores.php"><img src="res/provider.png" title="Proveedores"></a>
+    <a href="listadoSucursales.php"><img src="res/salesroom.png" title="Sucursales"></a>
+    <a href="listadoUsuarios.php"><img src="res/user.png" title="Usuarios"></a>
+    <a href="listadoOrdenes.php"><img src="res/order.png" title="Ordenes"></a>
+    <a href="listadoInventario.php"><img src="res/warehouse.png" title="Inventario"></a>
+</nav> 
