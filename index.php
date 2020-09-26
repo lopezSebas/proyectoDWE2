77d6ca8475cb701a959dcefadbeb033826cfb9ac
raggabatz <erik6594@gmail.com>
2020-09-26 02:13:31 -0600

    Log in out  and cart buttons

--- index.php
+++ index.php
@@ -2,7 +2,19 @@
 <article id="mainArticle">
     <div id="index">
         <div class="items-header">
-            <p>Search Nav</p>
+            <div class="search">
+                <input id="search" class="search" placeholder="Search">
+            </div>
+            <div class="log">
+                <a href="/proyectoDWE2/login.php">
+                    <button type="submit" class="btn-verde">
+                        <i class="fas fa-sign-in-alt"></i>
+                    </button>
+                </a>
+                <button type="submit" class="btn-verde">
+                    <i class="fas fa-sign-out-alt"></i>
+                </button>
+            </div>
         </div>
         <div class="shop-items">
             <?php include ("shop-item.php")?>
